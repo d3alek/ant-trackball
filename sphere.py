472129e304e7165f02f32bf7666f1c07f537f796
d3kod <xilunas@gmail.com>
2013-10-27 08:43:01 -0700

    Quaternions implemented

--- sphere.py
+++ sphere.py
@@ -8,10 +8,76 @@ import numpy as np
 import serial
 import subprocess
 
+TOLERANCE = 0.00001
+
+def normalizeQuaternion(quat):
+    global TOLERANCE
+    x, y, z, w = quat
+    mag2 = w * w + x * x + y * y + z * z
+    if np.abs(mag2) > TOLERANCE and np.abs(mag2 - 1) > TOLERANCE:
+        mag = sqrt(mag2)
+        x /= mag
+        y /= mag
+        z /= mag
+        w /= mag
+
+    return (x, y, z, w)
+
+
+def eulerToQuaternion(pitch, yaw, roll):
+    p = pitch*np.pi/(180*2.)
+    y = yaw*np.pi/(180*2.)
+    r = roll*np.pi/(180*2.)
+
+    sinp = np.sin(p)
+    siny = np.sin(y)
+    sinr = np.sin(r)
+    cosp = np.cos(p)
+    cosy = np.cos(y)
+    cosr = np.cos(r)
+
+    x = sinr * cosp * cosy - cosr * sinp * siny
+    y = cosr * sinp * cosy + sinr * cosp * siny
+    z = cosr * cosp * siny - sinr * sinp * cosy
+    w = cosr * cosp * cosy + sinr * sinp * siny
+
+    quat = normalizeQuaternion((x, y, z, w))
+
+    return quat 
+
+def multiplyQuaternions(quat1, quat2):
+    x1, y1, z1, w1 = quat1
+    x2, y2, z2, w2 = quat2
+    return (w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2,
+            w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2,
+            w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2,
+            w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2)
+
+def quaternionToMatrix(quat):
+    x, y, z, w = quat
+
+    x2 = x * x
+    y2 = y * y
+    z2 = z * z
+    xy = x * y
+    xz = x * z
+    yz = y * z
+    wx = w * x
+    wy = w * y
+    wz = w * z
+
+    return [1. - 2. * (y2 + z2), 2. * (xy - wz), 2. * (xz + wy), 0.,
+            2. * (xy + wz), 1. - 2. * (x2 + z2), 2. * (yz - wx), 0.,
+            2. * (xz - wy), 2 * (yz + wx), 1 - 2 * (x2 + y2), 0.,
+            0., 0., 0., 1.]
+
+
+
 
 SCREEN_SIZE = (800, 600)
 
 name = 'Ant Trackball'
+
 
 def resize(width, height):
     glViewport(0, 0, width, height)
@@ -42,35 +108,55 @@ def main():
               0,0,0,
               0,1,0)
     glPushMatrix()
-    rotX = rotY = 0
+    rot1X = rot1Y = 0
+    rot2X = rot2Y = 0
 
     subprocess.call(['sudo', 'chmod', '777', '/dev/ttyACM0'])
     ser = serial.Serial("/dev/ttyACM0", timeout=0)
 
     lines = []
     incomplete_line = None
-    
+
+    sensor1Pos = [0, 1/np.sqrt(2), -1/np.sqrt(2)]
+    sensor2Pos = [0, -1/np.sqrt(2), -1/np.sqrt(2)]
+
+    x1, y1, z1 = sensor1Pos
+    x2, y2, z2 = sensor2Pos
+
+    A = np.array([0, z1, -y1, -z1, 0, x1, y1, -x1, 0, 0, z2, -y2, -z2, 0, x2,
+                  y2, -x2, 0]).reshape([6,3])
+
+    U, s, V = np.linalg.svd(A)
+
+    globalQuat = eulerToQuaternion(0, 0, 0)
+   
     while True:
         lines = ser.readlines()
-        if incomplete_line:
-            lines[0] = incomplete_line + lines[0]
         if len(lines) > 0:
-            print "\n".join(lines)
+            if incomplete_line:
+                lines[0] = incomplete_line + lines[0]
+            #print "\n".join(lines)
             if lines[-1][-1] != '\n':
-                incomplete_line = lines[-1];
+                incomplete_line = lines[-1]
                 lines = lines[:-1]
             else:
                 incomplete_line = None
+        line = buffered_lines[0]
             for line in lines:
+                print line
                 ss = line.split()
                 x = y = 0
                 if len(ss) is not 6:
                     print "problem ss len is not 6!"
                 else:
-                    x = int(ss[2])
-                    y = int(ss[5])
-                rotX += x
-                rotY -= y
+                    x1 = int(ss[2])
+                    y1 = int(ss[3])
+                    x2 = int(ss[4])
+                    y2 = int(ss[5])
+                    rot1X += x1
+                    rot1Y -= y1
+                    rot2X += x2
+                    rot2Y += y2
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -79,35 +165,51 @@ def main():
             elif event.type == KEYDOWN:
                 if event.key == K_LEFT:
                     print "left" + str(i)
-                    rotX += -np.pi/10;
+                    rot2X += -np.pi/2;
                 if event.key == K_RIGHT: 
                     print "right" + str(i)
-                    rotX += np.pi/10;
+                    rot2X += np.pi/2;
                 if event.key == K_UP:
                     print "up" + str(i)
-                    rotY += np.pi/10;
+                    rot2Y += np.pi/2;
                 if event.key == K_DOWN:
                     print "down" + str(i)
-                    rotY += -np.pi/10;
-                if event.key == K_ESCAPE:
-                    pygame.event.post(pygame.event.Event(QUIT))
-        display(rotX, rotY)
+                    rot2Y += -np.pi/2;
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+        b = np.array([rot1X, rot1Y, 0, rot2X, rot2Y, 0])
+        w = U[:,0].dot(b)*V[:,0]/s[0]+U[:,1].dot(b)*V[:,1]/s[1]+U[:,2].dot(b)*V[:,2]/s[2]
+        # quaternion representing the change
+        #w = [rot2X, rot2Y, 0]
+        rot2X = rot2Y = 0
+        rot1X = rot1Y = 0
+        quat = eulerToQuaternion(w[0], w[1], w[2])
+
+        # rotate the global quaternion
+        globalQuat = multiplyQuaternions(globalQuat, quat)
+
+        display(globalQuat)
+        #display(quat)
+        #display((w[0], w[1], w[2]))
+
         pygame.display.flip()
         clock.tick(30)
     return
 
-def display(rotX, rotY):
+def display(quaternion):
+    #TODO rotate rotZ as well
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    currentModelViewMatrix = glGetDouble(GL_MODELVIEW_MATRIX)
-    glRotatef(rotX, currentModelViewMatrix[0][1], currentModelViewMatrix[1][1],
-              currentModelViewMatrix[2][1])
-    currentModelViewMatrix = glGetDouble(GL_MODELVIEW_MATRIX)
-    glRotatef(rotY, currentModelViewMatrix[0][0], currentModelViewMatrix[1][0],
-              currentModelViewMatrix[2][0])
+    #currentModelViewMatrix = glGetDouble(GL_MODELVIEW_MATRIX)
+    #glRotatef(quaternion[0], currentModelViewMatrix[0][1], currentModelViewMatrix[1][1], currentModelViewMatrix[2][1])
+    #currentModelViewMatrix = glGetDouble(GL_MODELVIEW_MATRIX)
+    #glRotatef(quaternion[1], currentModelViewMatrix[0][0], currentModelViewMatrix[1][0], currentModelViewMatrix[2][0])
+    glMultMatrixf(quaternionToMatrix(quaternion))
     color = [1.0,0.,0.,1.]
     glutWireSphere(2,20,20)
+    #glutWireCube(2)
     glPopMatrix()
     return
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': 
+    main()
